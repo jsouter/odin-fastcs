ef2b1f896338310581bfb1ab94a18583074864a7
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-04-12 13:50:42 +0000

    Fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,4 +6,5 @@ from odin_fastcs import __version__
 
 def test_cli_version():
     cmd = [sys.executable, "-m", "odin_fastcs", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
+    stdout = subprocess.check_output(cmd).decode().strip().split("\n")
+    assert __version__ in stdout
