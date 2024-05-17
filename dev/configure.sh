84bf215252c745820fe01e8be4375a17177d08ac
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-05-17 10:41:15 +0000

    Improve local dev deployment
    Copy templates to a gitignored directory for local deplyment
    so that it doesn't show as unstaged changes to the templates.

--- dev/configure.sh
+++ dev/configure.sh
@@ -11,14 +11,18 @@ ODIN_DATA=$1
 VENV=$2
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-SERVER="${SCRIPT_DIR}/stOdinServer.sh"
-FR="${SCRIPT_DIR}/stFrameReceiver1.sh"
-FR_CONFIG="${SCRIPT_DIR}/fr1.json"
-FP="${SCRIPT_DIR}/stFrameProcessor1.sh"
-FP_CONFIG="${SCRIPT_DIR}/fp1.json"
-META="${SCRIPT_DIR}/stMetaWriter.sh"
-LAYOUT="${SCRIPT_DIR}/layout.kdl"
+
+mkdir ${SCRIPT_DIR}/local
+cp ${SCRIPT_DIR}/templates/* ${SCRIPT_DIR}/local
+
+SERVER="${SCRIPT_DIR}/local/stOdinServer.sh"
+FR="${SCRIPT_DIR}/local/stFrameReceiver1.sh"
+FR_CONFIG="${SCRIPT_DIR}/local/fr1.json"
+FP="${SCRIPT_DIR}/local/stFrameProcessor1.sh"
+FP_CONFIG="${SCRIPT_DIR}/local/fp1.json"
+META="${SCRIPT_DIR}/local/stMetaWriter.sh"
+LAYOUT="${SCRIPT_DIR}/local/layout.kdl"
 
 sed -i "s+<ODIN_DATA>+${ODIN_DATA}+g" ${FR} ${FR_CONFIG} ${FP} ${FP_CONFIG}
 sed -i "s+<VENV>+${VENV}+g" ${SERVER} ${META}
-sed -i "s+<SCRIPT_DIR>+${SCRIPT_DIR}+g" ${LAYOUT}
+sed -i "s+<SCRIPT_DIR>+${SCRIPT_DIR}/local+g" ${LAYOUT}
--- dev/start.sh
+++ dev/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-ZELLIG_CONFIG="-l ${SCRIPT_DIR}/layout.kdl"
+ZELLIG_CONFIG="-l ${SCRIPT_DIR}/local/layout.kdl"
 
 zellij ${ZELLIG_CONFIG} || bash <(curl -L zellij.dev/launch) ${ZELLIG_CONFIG}
